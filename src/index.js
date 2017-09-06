4e1a821ce65d44b5915c1a4092431e33abd42122
Burakhan <burakhan.ozyol@eba.gov.tr>
2017-09-06 16:50:29 +0300

    Cleaning assest management

--- src/index.js
+++ src/index.js
@@ -1,22 +1,11 @@
 import _ from 'lodash';
-import './style.css';
-import Icon from './icon.png';
-import Data from './data.xml';
+
 
 function component() {
     let element = document.createElement('div');
 
     // Lodash, currently included via a script, is required for this line to work
-    // Lodash, now imported by this script
     element.innerHTML = _.join([ 'Hello', 'webpack' ], ' ');
-    element.classList.add('hello');
-
-    let myIcon = new Image();
-    myIcon.src = Icon;
-    element.appendChild(myIcon);
-
-    console.log(Data);
-
     return element;
 }
 
