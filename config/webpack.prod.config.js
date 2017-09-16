422fd5cabd32f3e34c383702b22eb59fc31e180d
Burakhan <burakhan.ozyol@eba.gov.tr>
2017-09-17 00:22:01 +0300

    complete code splitting

--- config/webpack.prod.config.js
+++ config/webpack.prod.config.js
@@ -4,12 +4,25 @@ const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 const common = require('./webpack.common.config.js');
 
 module.exports = merge(common, {
-    //devtool: 'source-map',
-    devtool: 'cheap-module-source-map',
+    devtool: 'source-map',
+    //devtool: 'cheap-module-source-map',
     plugins: [
+        /*
         new UglifyJSPlugin({
-            sourceMap: true
+            parallel: true,
+            uglifyOptions: {
+                ie8: false,
+                ecma: 6,
+                warnings: true,
+                mangle: true, // debug false
+                output: {
+                    comments: false,
+                    beautify: false,  // debug true
+                }
+            },
+            warnings: true,
         }),
+        */
         new webpack.DefinePlugin({
             'process.env': {
                 'NODE_ENV': JSON.stringify('production')
